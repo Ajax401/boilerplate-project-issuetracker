c5f443279a5f80e8b66cb6cac7362a39aac4cc22
Anton Pretorius <Ajax401@users.noreply.github.com>
2020-09-13 11:30:22 +0200

    Update mySchema.js

--- models/mySchema.js
+++ models/mySchema.js
@@ -1,1 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const currentSchema = ({
+ issue_title:{
+   type:String,
+   min:5,
+   max:20,
+   required:true
+       },
+ issue_text:{
+   type:String,
+   min:5,
+   max:200,
+   required:true
+      },
+ created_on:Date,
+ updated_on:Date,
+ created_by:{
+   type:String,
+   min:5,
+   max:20,
+   required:true
+      },
+ assigned_to:{
+   type:String,
+   min:5,
+   max:20
+      },
+ open:Boolean,
+ status_text:{
+   type:String,
+   min:5,
+   max:5
+ },
+  project:String
+})
+
+const mySchema = mongoose.model('feedback',currentSchema);
+
+module.exports = mySchema;
